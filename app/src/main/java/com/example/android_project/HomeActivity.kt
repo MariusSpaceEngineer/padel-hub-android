962fc49fccfddbd981cdfeda3e09297518927350
Jordi Timmers <timmersjordi@outlook.com>
2023-11-29 15:29:00 +0100

    Adde d HomeActivity that has navbar with multiple fragments

---
+++ app/src/main/java/com/example/android_project/HomeActivity.kt
@@ -0,0 +1,42 @@
+package com.example.android_project
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.android_project.fragments.HomeFragment
+import com.example.android_project.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity(){
+
+    private lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("HomeActivity", "Activity created")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener {
+            Log.d("HomeActivity", "Bottom navigation listener selected")
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+
+                else -> { false}
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+}
