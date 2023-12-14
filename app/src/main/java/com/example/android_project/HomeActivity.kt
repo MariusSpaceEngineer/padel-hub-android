7e3a710f8db691a8b9c2d02484d1a7fe3bad2147
MariusSpaceEngineer <marius.stoica@student.ap.be>
2023-12-14 14:26:32 +0100

    Create home fragment that let's you go to different fragments in the application

--- app/src/main/java/com/example/android_project/HomeActivity.kt
+++ app/src/main/java/com/example/android_project/HomeActivity.kt
@@ -4,22 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.android_project.fragments.BookACourtFragment
 import com.example.android_project.fragments.HomeFragment
 import com.example.android_project.fragments.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomeActivity : AppCompatActivity(){
+class HomeActivity : AppCompatActivity() {
 
-    private lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("HomeActivity", "Activity created")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
         loadFragment(HomeFragment())
-        bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
         bottomNav.setOnItemSelectedListener {
             Log.d("HomeActivity", "Bottom navigation listener selected")
             when (it.itemId) {
@@ -35,15 +37,15 @@ class HomeActivity : AppCompatActivity(){
                     loadFragment(ProfileFragment())
                     true
                 }
-
-                else -> { false}
+                else -> { false }
             }
         }
     }
 
-    private  fun loadFragment(fragment: Fragment){
+    private fun loadFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container,fragment)
+        transaction.replace(R.id.container, fragment)
         transaction.commit()
     }
+
 }
--- app/src/main/java/com/example/android_project/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/android_project/fragments/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import com.bumptech.glide.Glide
 import com.example.android_project.R
 
 class HomeFragment : Fragment() {
@@ -17,8 +20,41 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        val ivBookCourt: ImageView = view.findViewById(R.id.ivBookCourt)
+        Glide.with(this)
+            .load("https://firebasestorage.googleapis.com/v0/b/mobile-jordi-marius.appspot.com/o/book_padel.jpg?alt=media&token=ead2f34e-9449-46b3-9295-2f4f8cbd663a")
+            .into(ivBookCourt)
+
+        val ivSeeReservations: ImageView = view.findViewById(R.id.ivSeeReservations)
+        Glide.with(this)
+            .load("https://firebasestorage.googleapis.com/v0/b/mobile-jordi-marius.appspot.com/o/see_reservations.jpg?alt=media&token=ff2cdc65-6b1f-45f3-86fe-76f354994807")
+            .into(ivSeeReservations)
+
+        val ivJoinMatch: ImageView = view.findViewById(R.id.ivJoinMatch)
+        Glide.with(this)
+            .load("https://firebasestorage.googleapis.com/v0/b/mobile-jordi-marius.appspot.com/o/join_existing_match.jpg?alt=media&token=1166f734-f90a-4c0b-a8d0-7d498d704dfb")
+            .into(ivJoinMatch)
+
+        val cardBookCourt: CardView = view.findViewById(R.id.cardBookCourt)
+        cardBookCourt.setOnClickListener {
+            val fragment = BookACourtFragment() // Replace with your fragment class
+            parentFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        }
+
+        val cardSeeReservations: CardView = view.findViewById(R.id.cardSeeReservations)
+        cardSeeReservations.setOnClickListener {
+            val fragment = BookACourtFragment() // Replace with your fragment class
+            parentFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        }
+
+        val cardJoinMatch: CardView = view.findViewById(R.id.cardJoinMatch)
+        cardJoinMatch.setOnClickListener {
+            val fragment = BookACourtFragment() // Replace with your fragment class
+            parentFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        }
+
+        return view
+    }
 }
