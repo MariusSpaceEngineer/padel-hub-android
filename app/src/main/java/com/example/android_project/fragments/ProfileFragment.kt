ba11b7009ea6eb9a1edbf61ec2879be753b15829
Jordi Timmers <timmersjordi@outlook.com>
2023-12-14 11:19:17 +0100

    Merged profile-page with main

--- app/src/main/java/com/example/android_project/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/android_project/fragments/ProfileFragment.kt
@@ -1,25 +1,32 @@
 package com.example.android_project.fragments
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.android_project.LoginActivity
 import com.example.android_project.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(), PreferencesDialogFragment.PreferencesChangeListener {
 
     private var fullName: String = ""
+    private var bestHand: String = ""
+    private var courtPosition: String = ""
+    private var matchType: String = ""
+
+    private lateinit var logoutButton: Button
+    private lateinit var playButton: Button
+    private lateinit var editPreferences: Button
 
     private lateinit var auth: FirebaseAuth
 
@@ -28,6 +35,8 @@ class ProfileFragment : Fragment() {
         loadData()
     }
 
+
+
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +44,32 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(com.example.android_project.R.layout.fragment_profile, container, false)
+
+        logoutButton = view.findViewById(R.id.logoutButton)
+        playButton = view.findViewById(R.id.playButton)
+        editPreferences = view.findViewById(R.id.editPreferences)
+
+        editPreferences.setOnClickListener {
+            //implement preferences pop up here
+            val preferencesDialog = PreferencesDialogFragment()
+            preferencesDialog.setPreferencesChangeListener(this)
+            preferencesDialog.show(requireFragmentManager(), "PreferencesDialog")
+        }
+
+        logoutButton.setOnClickListener {
+            // Call signOut to log the user out
+            auth.signOut()
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        playButton.setOnClickListener {
+            val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNav)
+
+            // Select the 'Home' item in the BottomNavigationView
+            bottomNav.selectedItemId = R.id.home
+        }
 
         return view
 
@@ -51,10 +86,22 @@ class ProfileFragment : Fragment() {
                 .addOnSuccessListener { document ->
                     if (document != null) {
                         fullName = document.getString("name").toString()
+                        val preferences = document.get("preferences") as Map<*, *>
+                        bestHand = preferences["bestHand"].toString()
+                        courtPosition = preferences["courtPosition"].toString()
+                        matchType = preferences["matchType"].toString()
                         Log.d("ProfileFragment", "fullname is now $fullName")
-                        val v = view?.findViewById<TextView>(com.example.android_project.R.id.username)
-                        if (v != null) {
-                            v.text = fullName
+                        val usernameText = view?.findViewById<TextView>(com.example.android_project.R.id.username)
+                        val bestHandText = view?.findViewById<TextView>(com.example.android_project.R.id.bestHand)
+                        val courtPositionText = view?.findViewById<TextView>(com.example.android_project.R.id.courtPosition)
+                        val matchTypeText = view?.findViewById<TextView>(com.example.android_project.R.id.matchType)
+                        if (usernameText != null) {
+                            usernameText.text = fullName
+                        }
+                        if(bestHandText != null && courtPositionText != null && matchTypeText != null){
+                            bestHandText.text = bestHand
+                            courtPositionText.text = courtPosition
+                            matchTypeText.text = matchType
                         }
                         else{
                             Log.d("ProfileFragment", "v is null")
@@ -73,4 +120,10 @@ class ProfileFragment : Fragment() {
         Log.d("ProfileFragment", "Email: ${user?.email}")
         Log.d("ProfileFragment", "Name: ${user?.uid}")
     }
+
+    override fun onPreferencesChanged() {
+        loadData()
+    }
+
+
 }
